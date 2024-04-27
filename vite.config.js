ee301134c1a2ec0757a749d467d8b25f765e8a79
Caleb Likens <caleb@likens.dev>
2024-04-27 18:08:47 -0400

    adding portfolio placeholder

--- vite.config.js
+++ vite.config.js
@@ -8,6 +8,7 @@ export default defineConfig({
       input: {
         main: resolve(__dirname, 'index.html'),
         resume: resolve(__dirname, 'resume.html'),
+        portfolio: resolve(__dirname, 'portfolio.html'),
       },
     },
   },
